124b498d12b752bb2986840da3bc25d4ee05d927
Corey Lin <514971757@qq.com>
2021-12-10 15:12:29 +0800

    Update DataTypes.sol

--- contracts/protocol/libraries/types/DataTypes.sol
+++ contracts/protocol/libraries/types/DataTypes.sol
@@ -3,27 +3,40 @@ pragma solidity 0.6.12;
 
 library DataTypes {
   // refer to the whitepaper, section 1.1 basic concepts for a formal description of these properties.
+  // 有关这些属性的正式描述，请参阅白皮书1.1部分的基本概念。
   struct ReserveData {
     //stores the reserve configuration
+    //资金池配置
     ReserveConfigurationMap configuration;
     //the liquidity index. Expressed in ray
+    //流动性指数
     uint128 liquidityIndex;
     //variable borrow index. Expressed in ray
+    //可变借款指数
     uint128 variableBorrowIndex;
     //the current supply rate. Expressed in ray
+    //当前流动性供应率
     uint128 currentLiquidityRate;
     //the current variable borrow rate. Expressed in ray
+    //当前可变利率借款率
     uint128 currentVariableBorrowRate;
     //the current stable borrow rate. Expressed in ray
+    //当前稳定利率借款率
     uint128 currentStableBorrowRate;
+    //上次更新时间
     uint40 lastUpdateTimestamp;
     //tokens addresses
+    //aToken地址
     address aTokenAddress;
+    //稳定利率债务token地址，注：债务通证化，债务被表示为token
     address stableDebtTokenAddress;
+    //可变利率债务token地址
     address variableDebtTokenAddress;
     //address of the interest rate strategy
+    //利率策略的地址
     address interestRateStrategyAddress;
     //the id of the reserve. Represents the position in the list of the active reserves
+    //本资金池的id。表示在活跃资金池列表中的位置
     uint8 id;
   }
 
